78f9a468cd159ff565581f83d1cad1fb7490469a
Vinay Kalasannavar <vinay.kalasannavar@gmail.com>
2019-12-27 13:24:57 +0000

    Added a component with a list of orders.

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,21 @@
   <div id="app">
     <AppHeader />
     <HelloWorld msg="Welcome to Vinay's Vue.js App :)" />
+    <Orders />
   </div>
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
 import AppHeader from "./components/AppHeader.vue";
+import Orders   from "@/components/Orders"
 
 export default {
   name: "app",
   components: {
     HelloWorld,
-    AppHeader
+    AppHeader,
+    Orders
   }
 };
 </script>
---
+++ src/components/Orders.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <div>Here's the orders in the system:</div>
+    
+    <div v-for="order in orders" :key="order">
+      <span>OrderId: {{order.orderId}}</span>
+      <span>OrderNumber: {{order.orderNumber}}</span>
+      <span>OrderDate: {{order.orderDate}}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      key: "",
+      orders: [
+        {
+          orderId: "1",
+          orderNumber: "1001",
+          orderDate: "2019-12-01"
+        },
+        {
+          orderId: "2",
+          orderNumber: "1002",
+          orderDate: "2019-12-02"
+        },
+        {
+          orderId: "3",
+          orderNumber: "1003",
+          orderDate: "2019-12-03"
+        },
+        {
+          orderId: "4",
+          orderNumber: "1004",
+          orderDate: "2019-12-04"
+        },
+        {
+          orderId: "5",
+          orderNumber: "1005",
+          orderDate: "2019-12-05"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
